b0eea5d3c4605f26efcb1e0e44290e821831bbeb
Jeremy Gale <jeremy.gale@robotsandpencils.com>
2012-07-25 16:29:50 -0600

    Autogenerated User controller
    rails generate controller Users new --no-test-framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'static_pages#home'  
