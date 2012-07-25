e1c6a3bff45ca2c883f5ae03fbb8aeeb19fc5e81
Jeremy Gale <jeremy.gale@robotsandpencils.com>
2012-07-25 17:43:55 -0600

    Add some validation to user, and tests for it, but uniqueness isn't fully implemented yet. Not using a database index yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # One big problem with uniqueness - it's not unique! Can be racy. Needs
+  # to be unique at the database level. http://ruby.railstutorial.org/chapters/modeling-users#sec:uniqueness_validation
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
