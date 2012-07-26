a80a792abc8066ac55b53fb38a6553412f2936ff
Jeremy Gale <jeremy.gale@robotsandpencils.com>
2012-07-26 13:02:47 -0600

    Add a create action to actually save a user to DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
-  def new
+  def new # GET page to make a new user (signup)
   	@user = User.new
   end
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def create # POST create a new user
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
