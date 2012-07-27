a53a8d53396289c198d59a7c40a9db8705a1e29f
Jeremy Gale <jeremy.gale@robotsandpencils.com>
2012-07-26 19:15:11 -0600

    Add a signout feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
-  
+
   def current_user=(user)
     @current_user = user
   end
@@ -29,5 +29,10 @@ module SessionsHelper
 
     # ||= only sets if it is undefined
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def sign_out
+  	self.current_user = nil
+  	cookies.delete(:remember_token)
   end
 end
