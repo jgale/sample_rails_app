8c65bc150abcdeb003ab5538990e8b87c48aa829
Jeremy Gale <jeremy.gale@robotsandpencils.com>
2012-07-31 10:25:25 -0600

    Add validations for the relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -18,4 +18,7 @@ class Relationship < ActiveRecord::Base
   # we need to supply the class name User
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validates :followed_id, presence: true
+  validates :follower_id, presence: true
 end
